4dc1d3e297a419ab41dbe94446ef75ccc6e83775
techpassel <techpasselorg@gmail.com>
2022-10-11 02:05:39 +0530

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+//Note adding setup attribute is important as The setup attribute is a hint that makes Vue 
+//perform compile-time transforms that allow us to use Composition API with less boilerplate.
+import { ref, onMounted } from 'vue';
+
+const count = ref(0);
+
+const update = (type: string) => {
+    if (type == "inc")
+        count.value++;
+    else
+        count.value--;
+}
+
+
+onMounted(() => console.log(`Count is : ${count.value}`));
+
+</script>
+
+<template>
+    <p class="abc">Count is {{count}}</p>
+    <button class="inc" @click="update('inc')">Increase</button>
+    <button class="dec" @click="update('dec')">Decrease</button>
+</template>
+
+<style lang="scss" scoped>
+button {
+    border: none;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: white;
+    font-weight: 700;
+    cursor: pointer;
+}
+.inc {
+    background: red;
+}
+.dec {
+    background: green;
+    margin-left: 10px;
+}
+</style>
